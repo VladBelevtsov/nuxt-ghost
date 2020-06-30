346c4163c0e39dd16a8ca8669ca2f752870978bc
Vlad Bielievtsov <vladbelevtsovdaime@gmail.com>
2020-06-30 16:23:50 +0300

    first commit

---
+++ pages/_slug/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <main>
+            <h1>{{ post.title }}</h1>
+            <div class="content">
+                <div v-html="post.html">{{ post.html }}</div>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+import { getSinglePost } from '~/api/posts';
+export default {
+  async asyncData ({params}) {
+    const post = await getSinglePost(params.slug);
+    return { post: post }
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+@media only screen and (min-width: 768px) {
+    .content {
+        padding: 2em;
+    }
+}
+</style>
